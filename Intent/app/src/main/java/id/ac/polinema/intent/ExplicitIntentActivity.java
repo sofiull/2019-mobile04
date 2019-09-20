46c22f7490785565001ed71044d26097a9d29292
sofiull <sofiulfr@gmail.com>
2019-09-20 07:21:55 +0700

    Membuat Fungsi handle Submit Button

--- Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ExplicitIntentActivity.java
@@ -9,10 +9,23 @@ import android.widget.TextView;
 
 public class ExplicitIntentActivity extends AppCompatActivity {
 
+    //  Todo: Deklarasi Variable
+    private EditText inputText;
+    private TextView outputText;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_explicit_intent);
+
+        //  Todo: Bind Layout
+        inputText = findViewById(R.id.input_name);
+        outputText = findViewById(R.id.text_output);
     }
 
+    public void handleSubmit(View view) {
+        //  Todo: Handle Tombol Submit
+        String nama = inputText.getText().toString();
+        outputText.setText("Hello "+nama+", Congratulations!");
+    }
 }
