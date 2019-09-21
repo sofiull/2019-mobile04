5b01f55c796d31fd381f1925f2482640fed3814f
sofiull <sofiulfr@gmail.com>
2019-09-21 21:06:52 +0700

    Menyelesaikan Android Bundle

--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileBundleActivity.java
@@ -3,8 +3,13 @@ package id.ac.polinema.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class ProfileBundleActivity extends AppCompatActivity {
+    private TextView usernameText;
+    private TextView nameText;
+    private TextView AgeText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,10 +17,24 @@ public class ProfileBundleActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile_bundle);
 
         // TODO: bind here
-
+        usernameText = findViewById(R.id.text_username);
+        nameText = findViewById(R.id.text_name);
+        AgeText = findViewById(R.id.text_age);
         Bundle extras = getIntent().getExtras();
+
         if (extras != null) {
-            // TODO: display value here
+            // TODO: display value here (Menangkap data berdasarkan Key. exp = USERNAME_KEY)
+            //  Textview Error ketika melakukan setText dengan tipe integer :(
+
+            String username =  extras.getString("USERNAME_KEY");
+            String name =  extras.getString("NAME_KEY");
+            int age =  extras.getInt("AGE_KEY");
+
+            usernameText.setText(username);
+            nameText.setText(name);
+            AgeText.setText(Integer.toString(age));
+        }else{
+            Toast.makeText(this, "kosong", Toast.LENGTH_SHORT).show();
         }
     }
 }
