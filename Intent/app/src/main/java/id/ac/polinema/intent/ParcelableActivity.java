433c93f4134725578c3a1bdc637bca0feb4e0a21
sofiull <sofiulfr@gmail.com>
2019-09-25 09:58:00 +0700

    Melengkapi project menampilkan data dengan pendekatan parcelable

--- Intent/app/src/main/java/id/ac/polinema/intent/ParcelableActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ParcelableActivity.java
@@ -2,14 +2,39 @@ package id.ac.polinema.intent;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+
+import model.User;
 
 public class ParcelableActivity extends AppCompatActivity {
+    private EditText usernameText;
+    private EditText nameText;
+    private EditText ageText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_parcelable);
+
+        // TODO: bind here
+        usernameText = findViewById(R.id.input_username);
+        nameText = findViewById(R.id.input_name);
+        ageText = findViewById(R.id.input_age);
     }
 
+    public void handleSubmit(View view) {
+        String username = usernameText.getText().toString();
+        String name = nameText.getText().toString();
+        int age = Integer.parseInt(ageText.getText().toString());
+
+        User user = new User(username,name,age);
+
+        Intent intent = new Intent(this, ProfileParcelableActivity.class);
+        intent.putExtra("USER_KEY",user);
+
+        startActivity(intent);
+    }
 }
--- Intent/app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
+++ Intent/app/src/main/java/id/ac/polinema/intent/ProfileParcelableActivity.java
@@ -3,8 +3,15 @@ package id.ac.polinema.intent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import model.User;
 
 public class ProfileParcelableActivity extends AppCompatActivity {
+    private TextView usernameView;
+    private TextView nameView;
+    private TextView ageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -12,11 +19,26 @@ public class ProfileParcelableActivity extends AppCompatActivity {
         setContentView(R.layout.activity_profile_parcelable);
 
         // TODO: bind here
+        usernameView = findViewById(R.id.text_username);
+        nameView = findViewById(R.id.text_name);
+        ageView = findViewById(R.id.text_age);
 
         Bundle extras = getIntent().getExtras();
         if (extras != null) {
 
             // TODO: display value here
+            //  Textview Error ketika melakukan setText dengan tipe integer :(
+            User user = extras.getParcelable("USER_KEY");
+
+            String username = user.getUsername();
+            String name = user.getName();
+            int age = user.getAge();
+
+            usernameView.setText(username);
+            nameView.setText(name);
+            ageView.setText(Integer.toString(age));
+        }else {
+            Toast.makeText(this, "kosong", Toast.LENGTH_SHORT).show();
         }
     }
 }
